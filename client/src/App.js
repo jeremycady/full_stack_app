d6bf7bc5c4122cf9007e33040f1705c658824373
Jeremy Cady <jeremy@mx103.com>
2020-10-07 18:43:54 -0500

    add CreateCourse component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Redirect
+  Redirect, 
+  Switch
 } from "react-router-dom";
 
 // import Components
@@ -11,6 +12,8 @@ import Courses from './Components/Courses';
 import CourseDetail from './Components/CourseDetail';
 import UserSignIn from './Components/UserSignIn';
 import UserSignUp from './Components/UserSignUp';
+import CreateCourse from './Components/CreateCourse';
+import UpdateCourse from './Components/UpdateCourse';
 
 function App() {
 
@@ -21,10 +24,15 @@ function App() {
         <Route exact path="/">
           <Redirect to="/courses" />
         </Route>
-        <Route exact path="/courses" render={Courses}/>
-        <Route path="/courses/:id" render={CourseDetail}/>
-        <Route path ="/signin" render={UserSignIn}/>
-        <Route path ="/signup" render={UserSignUp}/>
+        <Switch>
+          <Route exact path="/courses" render={Courses}/>
+          <Route path="/courses/create" render={CreateCourse}/>
+          <Route exact path="/courses/:id" render={CourseDetail}/>
+          <Route path="/signin" render={UserSignIn}/>
+          <Route path="/signup" render={UserSignUp}/>
+          <Route path="/courses/:id/update" render={UpdateCourse}/>
+        </Switch>
+        
       </Router>
     </React.Fragment>
   );
