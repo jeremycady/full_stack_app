2f8b0b004a7d4cd18d88600cd8863e893c679c36
Jeremy Cady <jeremy@mx103.com>
2020-10-07 15:03:39 -0500

    add Header and global.css to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect} from 'react';
+import Header from './Components/Header';
 
 function App() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/courses')
-      .then(response => response.json())
-      .then(result => setData(result));
-    console.log(data);
-  },[]);
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/api/courses')
+  //     .then(response => response.json())
+  //     .then(result => setData(result));
+  //   console.log(data);
+  // },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Header />
   );
 }
 
