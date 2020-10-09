662b9284c99802ca461f750e79bd234c5673be33
Jeremy Cady <jeremy@mx103.com>
2020-10-09 08:44:55 -0500

    add ability to delete course in CourseDetail

--- client/src/Components/CourseDetail.js
+++ client/src/Components/CourseDetail.js
@@ -5,14 +5,18 @@ import Cookies from 'js-cookie';
 const CourseDetail = ({match}) => {
   const [course, setCourse] = useState({});
   const [isOwner, setIsOwner] = useState(true);
+  const [errors, setErrors] = useState({});
   const history = useHistory();
+  const emailAddress = JSON.parse(Cookies.get('authUser')).emailAddress;
+  const password = JSON.parse(Cookies.get('authUser')).password;
+
   const ownerError = () => {
     return (
       <div>
-        <h2 className="validation--errors--label">Update Error</h2>
+        <h2 className="validation--errors--label">Authorization Error</h2>
         <div className="validation-errors">
           <ul>
-            <li>You are not Authorized to update this course</li>
+            <li>You are not Authorized to make changes to this course</li>
           </ul>
         </div>
       </div>
@@ -30,19 +34,42 @@ const CourseDetail = ({match}) => {
 
   const ownerData = {...course.owner};
 
-  const updateHandler = () => {
-    if (ownerData.emailAddress === JSON.parse(Cookies.get('authUser')).emailAddress) {
+  const handleUpdate = () => {
+    if (ownerData.emailAddress === emailAddress) {
       history.push(`/courses/${id}/update`);
     } else {
       setIsOwner(false);
     }
   };
+
+  const checkStatus = (status) => {
+    if (status === 204) {
+      history.push(`/courses`);
+    }
+  };
+
+  async function handleDelete() {
+    if (ownerData.emailAddress === emailAddress) {
+      await fetch(`http://localhost:5000/api/courses/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization': 'Basic ' + btoa(`${emailAddress}:${password}`),
+      },
+      credentials: 'same-origin',
+    })
+    .then(res => checkStatus(res.status))
+    .catch(err => setErrors(err))
+    } else {
+      setIsOwner(false);
+    }
+  }
   
   return (
     <div>
         <div className="actions--bar">
           <div className="bounds">
-            <div className="grid-100"><span><span className="button" onClick={updateHandler}>Update Course</span><a className="button" href={`/courses/${course.id}/delete`}>Delete Course</a></span><a
+            <div className="grid-100"><span><span className="button" onClick={handleUpdate}>Update Course</span><span className="button" onClick={handleDelete}>Delete Course</span></span><a
                 className="button button-secondary" href="/">Return to List</a></div>
           </div>
         </div>
